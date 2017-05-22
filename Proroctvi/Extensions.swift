7f4663ed4b5e7657b30624f3fc3349c740b78b49
Jan Hermann <dev@hermann.in>
2017-05-22 21:25:02 +0200

    add MPL license

--- Proroctvi/Extensions.swift
+++ Proroctvi/Extensions.swift
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 extension Array {
     subscript(circ i: Int) -> Element {
         return self[i >= 0 ? i % self.count : (i % self.count)+self.count]
--- Proroctvi/main.swift
+++ Proroctvi/main.swift
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 var hráči: [Hráč] = (1...2).map { _ in HloupýHráč() }
 hráči[0].postava = Postava(povolání: .Druid)
 hráči[1].postava = Postava(povolání: .Zaklínačka)
